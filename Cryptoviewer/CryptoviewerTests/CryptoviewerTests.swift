91ebcf8cc6ccd3b601e41631804d77f6abf30a1b
Eduard Calero <Eduard.Calero@dxcfds.com>
2021-01-08 10:43:10 +0100

    Project creation

---
+++ Cryptoviewer/CryptoviewerTests/CryptoviewerTests.swift
@@ -0,0 +1,34 @@
+//
+//  CryptoviewerTests.swift
+//  CryptoviewerTests
+//
+//  Created by ESFDS on 08/01/2021.
+//  Copyright © 2021 Iflet.tech. All rights reserved.
+//
+
+import XCTest
+@testable import Cryptoviewer
+
+class CryptoviewerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
